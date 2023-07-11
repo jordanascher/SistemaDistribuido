3ba6d9ed605807ca0ca4ff89d7771e18bc6fffe4
Jordana <jordanaschercosta@hotmail.com>
2023-07-11 16:18:55 -0300

    alteração porta e comentário código

--- Cliente.py
+++ Cliente.py
@@ -2,20 +2,20 @@ from socket import *
 from threading import Thread
 
 myHost = "localhost"
-myPort = 50008
-
+myPort = 50007
+#socketClient cria um objeto  socket do cliente.
 socketClient = socket(AF_INET, SOCK_STREAM)
-
+# a função def conecta pelo localhost e porta 5007 ao servidor e manda uma msg conectando ao servidor
 def conecta ():
     print('Conectando ao servidor...')
     socketClient.connect((myHost, myPort))
     msgRcv = socketClient.recv(1024).decode('utf-8')
     print(msgRcv)
-
+# a função def desconecta e recebe um print saindo do chat quando desconecta e fecha o  socket, cliente vai comunicar o cliente e vai encerra a conexão
 def desconecta ():
     socketClient.close()
     print('Saindo do chat...')
-
+#vai enviar msg o cliente envia para o servidor
 def enviar(msg):
     socketClient.send(msg.encode('utf-8'))
 
@@ -30,7 +30,7 @@ def enviarMensagem():
                 desconecta()
                 break
 
-# thread for each roda ao mesmo tempo com o cliente escrita e de leitura que funcionam paralamente
+# thread vai enviar a msg para o servidor
 def main():
     conecta()
     thread1 = Thread(target=enviarMensagem)
--- Servidor.py
+++ Servidor.py
@@ -4,18 +4,18 @@ from datetime import datetime
 from threading import *
 
 host = 'localhost'
-port = 50008
-
+port = 50007
+#array que vai armazena cada cliente que entra
 connectionClientes = []
-
+#get pega a posição do array do cliente e o pop vai remover
 def deletarCliente(cliente):
     posicao = getPosicaoCliente(cliente)
     connectionClientes.pop(posicao)
-
+#append vai inserir na lista de clientes
 def inserirCliente(cliente):
     connectionClientes.append(cliente)
     return getPosicaoCliente(cliente)
-
+#ele vai pegar posição que o cliente esta no array 
 def getPosicaoCliente(cliente):
     i = 0
     posicaoCliente = None
@@ -28,10 +28,10 @@ def getPosicaoCliente(cliente):
         i = i + 1
 
     return posicaoCliente
-
+#formata cliente é só para exibir ele
 def formataCliente(posicao, cliente):
     return 'Cliente '+str(posicao+1) + '-' + str(cliente['ip']) + ':' + str(cliente['port'])
-
+#tupla posição 0 é o enderenço e a um é a porta
 def recebeCliente(connection, address):
     while True:
         cliente = {
@@ -61,20 +61,22 @@ def recebeCliente(connection, address):
             connection.close()
             break
         else:
-            msgServidor = 'Servidor diz: Mensagem recebida.'
+            msgServidor = 'Servidor diz: Mensagem recebida'
             connection.send(msgServidor.encode('utf-8'))
 
 def main():
     socketServer = socket(AF_INET, SOCK_STREAM)
-    # bind ele vai associar um endereço local a um soquete
+    # bind ele vai associar um endereço local a um soquete servidor 
     socketServer.bind((host, port))
     print("Iniciando servidor...")
+    #aguardar a requisição do cliente 
     socketServer.listen()
 
     while True:
         connection, address = socketServer.accept()
         # msgServidor = 'Servidor aceitou a conexão'
         # connection.send(msgServidor.encode('utf-8'))
+        # thread vai receber a função recebe clientes, ele vai receber o endereço do accept do socket,o servidor vai receber do cliente sempre que ele recebe uma msg.
         thread = Thread(target=recebeCliente, args=(connection, address))
         thread.start()
 
